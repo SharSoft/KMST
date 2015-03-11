39eef8643118355483cb0fbcfcc58edb4c6408b6
Simranjit Sekhon <ssekhon@umail.ucsb.edu>
2015-03-10 21:42:01 -0700

    int vertexes

--- MST.cpp
+++ MST.cpp
@@ -13,45 +13,10 @@
 #include <algorithm>
 using namespace std;
 
-class UnionFind
-{	
-	public:	
-
-	int* parent;
-	int* rank;
-
-	int find(int vertex) 
-	{
-		if (parent[vertex] == vertex) 
-			return parent[vertex];
-		else
-			return find(parent[vertex]);	
-	}
-
-	void Union(int root1, int root2)
-	{
-		if(rank[root1] > rank[root2])
-			parent[root2] = root1;
-		else if(rank[root2] > rank[root1])
-			parent[root1] = root2;
-		else
-		{
-			parent[root1] = root2;
-			rank[root2]++;
-		}
-	}
-
-	void Reset(int vertex)
-	{
-		parent[vertex] = vertex;
-		rank[vertex] = 0;
-	}	
-};
-
 struct Edge 
 {
-	string vertex1;
-	string vertex2;
+	int vertex1;
+	int vertex2;
 	int weight;
 };
 
@@ -62,6 +27,87 @@ struct Graph
 	int n;
 	int m;
 };
+
+int* parent;
+int* rank;
+
+int find(int vertex) 
+{
+	if (parent[vertex] == vertex) 
+		return parent[vertex];
+	else
+		return find(parent[vertex]);	
+}
+
+void Union(int root1, int root2)
+{
+	if(rank[root1] > rank[root2])
+		parent[root2] = root1;
+	else if(rank[root2] > rank[root1])
+		parent[root1] = root2;
+	else
+	{
+		parent[root1] = root2;
+		rank[root2]++;
+	}
+}
+
+void Reset(int vertex)
+{
+	parent[vertex] = vertex;
+	rank[vertex] = 0;
+}	
+
+void Kruskal(Graph g)
+{	
+	vector<Edge> MST;
+	int MSTweight = 0;
+
+	parent = new int[g.n];
+	rank = new int[g.n];
+
+	for(int i = 0; i <= g.n; i++)
+	{
+		parent[i] = i;
+		rank[i] = 0;
+	}
+
+	//................tester
+	for ( int i = 0; i <= g.n; i++) {
+            cout << "Set: " << parent[i] << " Height: " << rank[i] << "\n";
+        }
+
+	for (int i = 0; i < g.m; i++)
+	{
+		Edge tmp = g.edges[i];	
+		/*int root1 = find(tmp.vertex1);
+		int root2 = find(tmp.vertex2);
+		if (root1 != root2) 
+		{
+			MSTweight = MSTweight + e.weight;
+			MST.push_back(e);
+			if (rank[root1] > rank[root2]) 
+			{
+				Kset.parent[root2] = root1;
+				Kset.rank[root1]++;
+				cout << "Edge (" << e.vertex1 << "," << e.vertex2 << ") successfully inserted" << "\n";
+			} 
+			else 
+			{
+				Kset.parent[root1] = root2;
+				Kset.rank[root2]++;
+				cout << "Edge (" << e.vertex1 << "," << e.vertex2 << ") successfully inserted" << "\n";
+			}
+		}
+		else
+			cout << "Edge (" << g.e.vertex1 << "," << g.e.vertex2 << ") creates cycle" << "\n";
+	*/}
+
+	/*for (Edge e : MST) 
+	{
+		cout << e.vertex1 << " " << e.vertex2 << endl;
+	}*/
+}
 
 //prints current array of edges
 void printGraph(Edge* e, int m, int n, int* v)
@@ -92,54 +138,6 @@ void sort(Edge* e, int m)
 	sort(e, e + m, edgecompare);
 }
 
-void Kruskal(Graph g)
-{	
-	vector<Edge> MST;
-	int MSTweight = 0;
-
-	UnionFind Kset;
-
-	Kset.parent = new int[g.n];
-	Kset.rank = new int[g.n];
-
-	for(int i = 0; i <= g.n; i++)
-	{
-		Kset.parent[i] = i;
-		Kset.rank[i] = 0;
-	}
-
-	for (int i = 0; i < g.m; i++)
-	{
-		Edge tmp = g.edges[i];	
-		/*int root1 = Kset.find(tmp.vertex1);
-		int root2 = Kset.find(tmp.vertex2);
-		if (root1 != root2) 
-		{
-			MSTweight = MSTweight + e.weight;
-			MST.push_back(e);
-			if (rank[root1] > rank[root2]) 
-			{
-				Kset.parent[root2] = root1;
-				Kset.rank[root1]++;
-				cout << "Edge (" << e.vertex1 << "," << e.vertex2 << ") successfully inserted" << "\n";
-			} 
-			else 
-			{
-				Kset.parent[root1] = root2;
-				Kset.rank[root2]++;
-				cout << "Edge (" << e.vertex1 << "," << e.vertex2 << ") successfully inserted" << "\n";
-			}
-		}
-		else
-			cout << "Edge (" << g.e.vertex1 << "," << g.e.vertex2 << ") creates cycle" << "\n";*/
-	}
-
-	/*for (Edge e : MST) 
-	{
-		cout << e.vertex1 << " " << e.vertex2 << endl;
-	}*/
-}
-
 int main()
 {  
 	Graph g;
@@ -168,11 +166,20 @@ int main()
 	for(int i = 0; i < g.m; i++)
 	{
 		int w = 0;
+		int v1 = 0;
+		int v2 = 0;
+		string tmpv1;
+		string tmpv2;
 		string tmpw;
 	
-		getline(cin, g.edges[i].vertex1,' ');
+		//getline(cin, g.edges[i].vertex1,' ');
+		getline(cin,tmpv1);
+		v1 = atoi(tmpv1.c_str());
+		g.edges[i].vertex1 = v1;
 	
-		getline(cin, g.edges[i].vertex2,' ');
+		//getline(cin, g.edges[i].vertex2,' ');
+		v2 = atoi(tmpv2.c_str());
+		g.edges[i].vertex2 = v2;
 	
 		getline(cin,tmpw);
 		w = atoi(tmpw.c_str());
@@ -183,7 +190,7 @@ int main()
 
 	printGraph(g.edges, g.m, g.n, g.vertices);
 
-	//Kruskal(g);
+	Kruskal(g);
 
 	return 1;
 }
